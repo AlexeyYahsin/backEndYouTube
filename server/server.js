6171fe1f8767b86b1a58860a0a01734f28448911
alexeyYashin <alexeyyashin@mail.ru>
2023-02-08 23:00:59 +0400

    lesson05

--- server/server.js
+++ server/server.js
@@ -1,2 +1,19 @@
-const counter = 0
-console.log(counter);
+const http = require("http")
+let requestsCount = 0
+ 
+http.createServer(function(request, response){
+    requestsCount++
+
+    switch(request.url){
+        case('/students'):
+            response.write('students ')
+            break
+        case('/cousrses'):
+            response.write('cousrses ')
+            break
+        default:
+            response.write('404 not found ')
+    }
+    response.write("Count:" + requestsCount)
+    response.end()
+}).listen(3003)
