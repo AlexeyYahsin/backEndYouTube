9f52a3a43aa125ca82f2721faedc792d5df26813
alexeyYashin <alexeyyashin@mail.ru>
2023-02-21 23:23:06 +0400

    l9

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,14 +6,62 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3003;
-app.get('/', (req, res) => {
-    res.send('Hello World!!!!!!!!!');
+const jsonBodyMidlleware = express_1.default.json();
+app.use(jsonBodyMidlleware);
+const db = {
+    courses: [
+        { id: 1, title: "front-end" },
+        { id: 2, title: "back-end" },
+        { id: 3, title: "automation qa" },
+        { id: 4, title: "devops" },
+    ],
+};
+app.get('/courses', (req, res) => {
+    let foundCourse = db.courses;
+    if (req.query.title) {
+        foundCourse = foundCourse.filter(c => c.title.indexOf(req.query.title) > -1);
+    }
+    res.json(foundCourse);
 });
-app.get('/samurais', (req, res) => {
-    res.send('Hello samurais!!!!!');
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find((x) => x.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(foundCourse);
 });
-app.post('/samurais', (req, res) => {
-    res.send('We have created samurai');
+app.post('/courses', (req, res) => {
+    const checkedTitle = req.body.title.trim();
+    if (!checkedTitle) {
+        res.sendStatus(400);
+        return;
+    }
+    const createdCourse = {
+        id: +(new Date()),
+        title: req.body.title
+    };
+    db.courses.push(createdCourse);
+    res
+        .status(201)
+        .json(createdCourse);
+});
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter((x) => x.id !== +req.params.id);
+    res.sendStatus(204);
+});
+app.put('/courses/:id', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(404);
+        return;
+    }
+    const foundCourse = db.courses.find((x) => x.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    foundCourse.title = req.body.title;
+    res.json(foundCourse);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,83 @@
 import express from 'express'
 const app = express()
 const port = 3003
+const jsonBodyMidlleware = express.json()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!')
+app.use(jsonBodyMidlleware)
+
+const db = {
+  courses: [
+    { id: 1, title: "front-end" },
+    { id: 2, title: "back-end" },
+    { id: 3, title: "automation qa" },
+    { id: 4, title: "devops" },
+  ],
+};
+
+app.get('/courses', (req, res) => {
+  let foundCourse = db.courses
+
+  if(req.query.title){
+    foundCourse = foundCourse.filter(c => c.title.indexOf(req.query.title as string) > -1)
+  }
+  
+  res.json(foundCourse)
 })
-app.get('/samurais', (req, res) => {
-  res.send('Hello samurais!!!!!')
+
+app.get('/courses/:id', (req, res) => {
+  const foundCourse = db.courses.find((x) => x.id === +req.params.id);
+  
+  if(!foundCourse){
+    res.sendStatus(404)
+    return
+  }
+
+  res.json(foundCourse)
 })
-app.post('/samurais', (req, res) => {
-  res.send('We have created samurai')
+
+app.post('/courses', (req, res) => {
+
+  const checkedTitle = req.body.title.trim()
+
+  if(!checkedTitle){
+    res.sendStatus(400)
+    return
+  }
+
+  const createdCourse = {
+    id: +(new Date()),
+    title: req.body.title
+  }
+
+  db.courses.push(createdCourse)
+
+  res
+    .status(201)
+    .json(createdCourse)
+})
+
+app.delete('/courses/:id', (req, res) => {
+  db.courses = db.courses.filter((x) => x.id !== +req.params.id);
+  
+  res.sendStatus(204)
+})
+
+app.put('/courses/:id', (req, res) => {
+  if(!req.body.title){
+    res.sendStatus(404)
+    return
+  }
+  
+  const foundCourse = db.courses.find((x) => x.id === +req.params.id);
+  
+  if(!foundCourse){
+    res.sendStatus(404)
+    return
+  }
+
+  foundCourse.title = req.body.title
+
+  res.json(foundCourse)
 })
 
 app.listen(port, () => {
